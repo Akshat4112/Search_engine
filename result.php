1b69c245319b0e8ca4dee9aa8d6207c3fd9ff916
Akshat <akshat41121995@gmail.com>
2017-07-16 12:20:32 +0530

    ADMIN STRUCTURE CREATED

--- result.php
+++ result.php
@@ -1,8 +1,29 @@
 <html>
 <head>
     <title>Results</title>
+    <style type="text/css">
+.results{
+    margin-left:10%;
+    margin-right:10%;
+    margin-top: 20px;
+}
+    </style>
 </head>
-<body>
-This is Results
+<body bgcolor="aqua">
+<form action="result.php" method="get">
+    <span><b>Write Your Keywrod: </b></span>
+    <input type="text" name="user_keyword" size="120"/>
+    <input type="submit" name="search" value="Search Now">
+</form>
+<?php
+if(isset($_GET['search'])){
+    $get_value=$_GET['user_query'];
+
+    echo "<div class='results'>
+$get_value;
+</div>";
+}
+?>
+
 </body>
 </html>
